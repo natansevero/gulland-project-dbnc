f2b2bc6f1441fe3b23b2f5d9675105419a984153
natansevero <natanseverolp@gmail.com>
2017-04-11 12:34:58 -0300

    Config render de página de login

--- libs/middlewares.js
+++ libs/middlewares.js
@@ -7,9 +7,9 @@ const path = require('path');
 module.exports = app => {
   app.set('port', process.env.NODE_ENV || 3000);
   app.set('view engine', 'ejs');
-  app.set('views', path.join(__dirname, 'views'));
+  app.set('views', path.join(__dirname, '../views'));
 
-  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.static(path.join(__dirname, '../public')));
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(expressValidator());
   app.use(expressSession({
