7efaab8858d006d398e182f84bdb56ef73469a25
natansevero <natanseverolp@gmail.com>
2017-04-11 02:43:38 -0300

    Organizando arquitetura do projeto

---
+++ README.md
@@ -0,0 +1,2 @@
+# Gulland Project - Sistema Web para criação e administração de websites
+## Projeto para obtenção de nota da disciplina de Bando de Dados II do curso de Análise e Desenvolvimento de Sistemas - IFPB - Campus Cajazeiras
---
+++ libs/middlewares.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const expressSession = require('express-session');
+const expressValidator = require('express-validator');
+const path = require('path');
+
+module.exports = app => {
+  app.set('port', process.env.NODE_ENV || 3000);
+  app.set('view engine', 'ejs');
+  app.set('views', path.join(__dirname, 'views'));
+
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(expressValidator());
+  app.use(expressSession({
+    secret: 'appbsied',
+    resave: false,
+    saveUninitialized: false
+  }));
+}
