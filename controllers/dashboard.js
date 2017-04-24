362833e77b5b123eb539817718ca1ce5f6f2e968
natansevero <natanseverolp@gmail.com>
2017-04-23 21:45:54 -0300

    Finalizando inserir, atualizar e deletar dados de portifólio

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,8 +1,24 @@
 module.exports = app => {
+  let PortifolioModel = app.models.portifolio;
+
   let DashboardController = {
     verificaDash: (req, res) => {
       if(req.query.tipo == 'blog') return res.render('dashboard/blog')
-      else if(req.query.tipo == 'portifolio') return res.render('dashboard/portifolio')
+      else if(req.query.tipo == 'portifolio') {
+        let _id = req.session.usuario.id;
+        PortifolioModel.find({ id_usuario: _id }, (err, result) => {
+          if(err) return res.redirect('/');
+          else if(result.length <= 0) {
+            PortifolioModel.create({ id_usuario: _id }, (err, result) => {
+              if(err) return res.redirect('/');
+              else return res.render('dashboard/portifolio', { secao: [] })
+            })
+          } else {
+            let secao = result[0].secao;
+            return res.render('dashboard/portifolio', { secao: secao.reverse() })
+          }
+        })
+      }
       else return res.redirect('/')
     },
 
@@ -11,8 +27,27 @@ module.exports = app => {
     },
 
     PortifolioDash: {
-      create: (req, res) => {
+      render_site: (req, res) => {
+        res.render('site/site_portifolio');
+      },
 
+      view_create: (req, res) => {
+        res.render('dashboard/add_portifolio', { validacao: null });
+      },
+
+      update_view: (req, res) => {
+        let dados_secao = req.query;
+        res.render('dashboard/update_portifolio', { validacao: null, dados_secao: dados_secao });
+      },
+
+      create: (req, res) => {
+        let secao = req.body;
+        let _id = req.session.usuario.id;
+
+        PortifolioModel.update({ id_usuario: _id }, { $push: { secao: secao } }, (err, result) => {
+          if(err) return res.render('dashboard/add_portifolio', { validacao: "Algo de errado aconteceu!" });
+          else return res.redirect('/dashboard?tipo=portifolio');
+        });
       },
 
       retrieve: (req, res) => {
@@ -24,11 +59,35 @@ module.exports = app => {
       },
 
       update: (req, res) => {
+        let id_usuario = req.session.usuario.id;
 
+        PortifolioModel.findOne({ id_usuario: id_usuario }, (err, user) => {
+          if(err) return res.redirect('/dashboard?tipo=portifolio');
+          user.secao.forEach((s, index) => {
+            if(s._id == req.body._id) {
+              user.secao[index].titulo = req.body.titulo;
+              user.secao[index].descricao = req.body.descricao;
+            }
+          });
+
+          user.save((err, result) => {
+            if(err) {
+              console.log("erro:", err);
+              return res.redirect('/dashboard?tipo=portifolio');
+            }
+            else return res.redirect('/dashboard?tipo=portifolio');
+          })
+        })
       },
 
       delete: (req, res) => {
+        let id_usuario = req.session.usuario.id;
+        let id_secao = req.params.id_secao;
 
+        PortifolioModel.update({ id_usuario: id_usuario }, { $pull: { secao: { _id: id_secao } } }, (err, result) => {
+          if(err) return res.redirect('/dashboard?tipo=portifolio');
+          else return res.redirect('/dashboard?tipo=portifolio');
+        })
       }
     }
   }
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -5,8 +5,20 @@ module.exports = app => {
   // Rotas principal
   app.get('/dashboard', auth, DashboardController.verificaDash);
 
-  // Rotas específicas para Blog
+  // ----- Rotas específicas para Blog ------
 
-  // Rotas específicas para Portifólio
+  // ----- Rotas específicas para Portifólio ------
+  // Render Complet Site
+  app.get('/site_portifolio', auth, DashboardController.PortifolioDash.render_site);
+  // Render view for add porfifolio
+  app.get('/add_portifolio', auth, DashboardController.PortifolioDash.view_create);
+  // Route create in portifolio
+  app.post('/create_port', auth, DashboardController.PortifolioDash.create);
 
+  // Route delete in portifolio
+  app.get('/delete_port/:id_secao', auth, DashboardController.PortifolioDash.delete);
+
+  // Render view for update portifolio
+  app.get('/update_portifolio', auth, DashboardController.PortifolioDash.update_view);
+  app.post('/update_port', auth, DashboardController.PortifolioDash.update);
 }
